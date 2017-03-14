0dc9035840c80b4346d964a60c7e795c25c85645
WraithCadmus <wraithcadmus@users.noreply.github.com>
2017-03-14 19:56:44 +0000

    Now have a list of all files

--- extractor.py
+++ extractor.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python
+
+import os
+import glob
+
+# Paths containing icons we want
+
+sources = ['Stellaris/gfx/interface/icons/ethics/',
+           'Stellaris/gfx/interface/icons/governments/',
+           'Stellaris/gfx/interface/icons/traits/',
+           'Stellaris/gfx/interface/icons/traits/leader_traits/']
+
+# Test echo of paths
+# print(sources)
+
+# Compiling a list of every dds file we're after
+
+sourcefiles = []
+
+for s in sources:
+    sourcefiles.extend(glob.glob(s + '*.dds'))
+
+print sourcefiles
